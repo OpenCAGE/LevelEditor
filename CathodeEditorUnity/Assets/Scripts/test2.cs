cbac750d6317801c01f54f59f5b31309d085012c
Matt Filer <matt.filer@ultraleap.com>
2021-03-24 13:09:33 +0000

    Continued

--- CathodeEditorUnity/Assets/Scripts/test2.cs
+++ CathodeEditorUnity/Assets/Scripts/test2.cs
@@ -4,29 +4,103 @@ using UnityEngine;
 using TestProject;
 using System.IO;
 using System;
+using CATHODE;
+using System.Linq;
 
 public class test2 : MonoBehaviour
 {
     alien_level Result = new alien_level();
 
+    CATHODE.CommandsPAK commandsPAK;
+    CATHODE.RenderableElementsBIN redsBIN;
+
     void Start()
     {
-        string levelPath = @"G:\SteamLibrary\steamapps\common\Alien Isolation\DATA\ENV\PRODUCTION\BSP_TORRENS";
+        string levelPath = @"C:\Users\MattFiler\Downloads\BSP_TORRENS\BSP_TORRENS";
 
         Result.ModelsCST = File.ReadAllBytes(levelPath + "/RENDERABLE/LEVEL_MODELS.CST");
         Result.ModelsMTL = TestProject.File_Handlers.Models.ModelsMTL.Load(levelPath + "/RENDERABLE/LEVEL_MODELS.MTL", Result.ModelsCST);
         Result.ModelsBIN = TestProject.File_Handlers.Models.ModelBIN.Load(levelPath + "/RENDERABLE/MODELS_LEVEL.BIN");
         Result.ModelsPAK = TestProject.File_Handlers.Models.ModelPAK.Load(levelPath + "/RENDERABLE/LEVEL_MODELS.PAK");
+
+
+        commandsPAK = new CATHODE.CommandsPAK(levelPath + @"\WORLD\COMMANDS.PAK");
+        redsBIN = new CATHODE.RenderableElementsBIN(levelPath + @"\WORLD\REDS.BIN");
+        RecursiveLoad(commandsPAK.EntryPoints[0], new PosAndRot());
     }
 
-    private GameObject LoadModel(int EntryIndex)
+    private void RecursiveLoad(CathodeFlowgraph flowgraph, PosAndRot stackedTransform)
     {
-        Debug.Log("---");
-        Debug.Log("LOADING MODEL AT INDEX " + EntryIndex);
+        List<CathodeNodeEntity> models = flowgraph.nodes; //TODO: reimplement model filtering from test.cs (nodedb broken)
+        foreach (CathodeNodeEntity node in models)
+        {
+            PosAndRot thisNodePos = GetTransform(node) + stackedTransform;
+            LoadREDS(node, flowgraph, thisNodePos);
+        }
+
+        foreach (CathodeNodeEntity node in flowgraph.nodes)
+        {
+            CathodeFlowgraph nextCall = commandsPAK.GetFlowgraph(node.nodeType);
+            if (nextCall == null) continue;
+            Debug.Log(GetTransform(node).position);
+            RecursiveLoad(nextCall, stackedTransform + GetTransform(node));
+        }
+    }
+    private PosAndRot GetTransform(CathodeNodeEntity node)
+    {
+        PosAndRot toReturn = new PosAndRot();
+        foreach (CathodeParameterReference paramRef in node.nodeParameterReferences)
+        {
+            CathodeParameter param = commandsPAK.GetParameter(paramRef.offset);
+            if (param == null) continue;
+            if (param.dataType != CathodeDataType.POSITION) continue;
+            Vec3 position = ((CathodeTransform)param).position;
+            Vec3 rotation = ((CathodeTransform)param).rotation;
+            toReturn.position = new Vector3(position.x, position.y, position.z);
+            toReturn.rotation = new Vector3(rotation.x, rotation.y, rotation.z);
+        }
+        return toReturn;
+    }
+    private void LoadREDS(CathodeNodeEntity node, CathodeFlowgraph flowgraph, PosAndRot thisNodePos)
+    {
+        //If has a renderable element, try create it
+        byte[] resourceID = GetResource(node);
+        if (resourceID == null) return;
+        CathodeResourceReference resRef = flowgraph.GetResourceReferenceByID(resourceID);
+        if (resRef == null || resRef.entryType != CathodeResourceReferenceType.RENDERABLE_INSTANCE) return;
+        List<RenderableElement> redsList = new List<RenderableElement>();
+        for (int p = 0; p < resRef.entryCountREDS; p++) redsList.Add(redsBIN.GetRenderableElement(resRef.entryIndexREDS + p));
+        if (resRef.entryCountREDS != redsList.Count || redsList.Count == 0) return; //TODO: handle this nicer
+        foreach (RenderableElement renderable in redsList)
+        {
+            LoadModel(renderable.model_index, thisNodePos);
+        }
+    }
+    private byte[] GetResource(CathodeNodeEntity node)
+    {
+        foreach (CathodeParameterReference paramRef in node.nodeParameterReferences)
+        {
+            CathodeParameter param = commandsPAK.GetParameter(paramRef.offset);
+            if (param == null) continue;
+            if (param.dataType != CathodeDataType.SHORT_GUID) continue;
+            return ((CathodeResource)param).resourceID;
+        }
+        return null;
+    }
+
+    private GameObject LoadModel(int EntryIndex, PosAndRot thisNodePos)
+    {
+        if (EntryIndex < 0 || EntryIndex >= Result.ModelsPAK.Models.Count)
+        {
+            Debug.LogWarning("Asked to load model at index " + EntryIndex + ", which is out of bounds!");
+            return new GameObject();
+        }
+
         alien_pak_model_entry ChunkArray = Result.ModelsPAK.Models[EntryIndex];
-        Debug.Log("CHUNK COUNT: " + ChunkArray.Header.ChunkCount);
 
         GameObject ThisModel = new GameObject();
+        ThisModel.transform.position = thisNodePos.position;
+        ThisModel.transform.rotation = Quaternion.Euler(thisNodePos.rotation);
 
         for (int ChunkIndex = 0; ChunkIndex < ChunkArray.Header.ChunkCount; ++ChunkIndex)
         {
@@ -39,32 +113,28 @@ public class test2 : MonoBehaviour
 
             BinaryReader Stream = new BinaryReader(new MemoryStream(ChunkArray.Chunks[ChunkIndex]));
 
-            int VertexArrayCount = 1;
-            alien_vertex_buffer_format_element[] Elements = new alien_vertex_buffer_format_element[256];
-            Elements[0] = VertexInput.Elements[0];
-            int[] ElementCounts = new int[256];
-            for (int ElementIndex = 0; ElementIndex < VertexInput.ElementCount; ++ElementIndex)
+            List<List<alien_vertex_buffer_format_element>> Elements = new List<List<alien_vertex_buffer_format_element>>();
+            alien_vertex_buffer_format_element ElementHeader = new alien_vertex_buffer_format_element();
+            foreach (alien_vertex_buffer_format_element Element in VertexInput.Elements)
             {
-                alien_vertex_buffer_format_element Element = VertexInput.Elements[ElementIndex];
-                if (VertexArrayCount - 1 != Element.ArrayIndex)
+                if (Element.ArrayIndex == 0xFF)
                 {
-                    Elements[VertexArrayCount++] = Element;
+                    ElementHeader = Element;
+                    continue;
                 }
-                ElementCounts[VertexArrayCount - 1]++;
-            }
 
-            //Debug.Log(VertexArrayCount);
+                while (Elements.Count - 1 < Element.ArrayIndex) Elements.Add(new List<alien_vertex_buffer_format_element>());
+                Elements[Element.ArrayIndex].Add(Element);
+            }
+            Elements.Add(new List<alien_vertex_buffer_format_element>() { ElementHeader });
 
             List<UInt16> InIndices = new List<UInt16>();
             List<Vector3> InVertices = new List<Vector3>();
             List<Vector3> InNormals = new List<Vector3>();
 
-            for (int VertexArrayIndex = 0; VertexArrayIndex < VertexArrayCount; ++VertexArrayIndex)
+            for (int VertexArrayIndex = 0; VertexArrayIndex < Elements.Count; ++VertexArrayIndex)
             {
-                Debug.Log(Stream.BaseStream.Position + " - " + Stream.BaseStream.Length);
-
-                int ElementCount = ElementCounts[VertexArrayIndex];
-                alien_vertex_buffer_format_element Inputs = Elements[VertexArrayIndex];
+                alien_vertex_buffer_format_element Inputs = Elements[VertexArrayIndex][0];
                 if (Inputs.ArrayIndex == 0xFF)
                 {
                     for (int i = 0; i < Model.IndexCount; i++)
@@ -76,9 +146,9 @@ public class test2 : MonoBehaviour
                 {
                     for (int VertexIndex = 0; VertexIndex < Model.VertexCount; ++VertexIndex)
                     {
-                        for (int ElementIndex = 0; ElementIndex < ElementCount; ++ElementIndex)
+                        for (int ElementIndex = 0; ElementIndex < Elements[VertexArrayIndex].Count; ++ElementIndex)
                         {
-                            alien_vertex_buffer_format_element Input = Elements[VertexArrayIndex + ElementIndex];
+                            alien_vertex_buffer_format_element Input = Elements[VertexArrayIndex][ElementIndex];
                             switch (Input.VariableType)
                             {
                                 case alien_vertex_input_type.AlienVertexInputType_v3:
@@ -184,20 +254,11 @@ public class test2 : MonoBehaviour
 
             GameObject ThisModelPart = new GameObject();
             ThisModelPart.transform.parent = ThisModel.transform;
+            ThisModelPart.transform.localScale = new Vector3(Model.ScaleFactor, Model.ScaleFactor, Model.ScaleFactor);
             ThisModel.name = Result.ModelsBIN.ModelFilePaths[BINIndex];
             ThisModelPart.name = Result.ModelsBIN.ModelLODPartNames[BINIndex] + "(" + Result.ModelsMTL.MaterialNames[Model.MaterialLibraryIndex] + ")"; 
 
             if (InVertices.Count == 0) continue;
-
-            int maxind = -int.MaxValue;
-            for (int i = 0; i < InIndices.Count; i++)
-            {
-                //Debug.Log(indiciesConv[i]);
-                if (InIndices[i] > maxind) maxind = InIndices[i];
-            }
-
-            Debug.Log("Indices: " + InIndices.Count + ", Vertices: " + InVertices.Count + ", MaxInd: " + maxind);
-            Debug.Log("---");
 
             Mesh thisMesh = new Mesh();
             thisMesh.SetVertices(InVertices);
@@ -214,9 +275,9 @@ public class test2 : MonoBehaviour
         return ThisModel;
     }
 
-    // Update is called once per frame
+    /*
     GameObject currentMesh = null;
-    int currentMeshIndex = 50;
+    int currentMeshIndex = 550;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
@@ -226,6 +287,7 @@ public class test2 : MonoBehaviour
             currentMeshIndex++;
         }
     }
+    */
 
     public void Align(BinaryReader reader, int val)
     {
