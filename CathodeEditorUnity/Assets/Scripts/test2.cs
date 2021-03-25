fbe3f29afcbebb48e48e7cee33424e42f7f26b93
MattFiler <matthew.filer@hotmail.co.uk>
2021-03-25 01:09:02 +0000

    Further texture tests

--- CathodeEditorUnity/Assets/Scripts/test2.cs
+++ CathodeEditorUnity/Assets/Scripts/test2.cs
@@ -30,13 +30,14 @@ public class test2 : MonoBehaviour
         Result.ShadersIDXRemap = TestProject.File_Handlers.Shaders.IDXRemap.Load(levelPath + "/RENDERABLE/LEVEL_SHADERS_DX11_IDX_REMAP.PAK");
     }
 
-    private void LoadTexture(int EntryIndex)
+    private Texture2D LoadTexture(int EntryIndex, int paktype = 0)
     {
         alien_textures AlienTextures = Result.LevelTextures; //todo; pass as param
+        if (paktype == 2) AlienTextures = Result.GlobalTextures;
         if (EntryIndex < 0 || EntryIndex >= AlienTextures.PAK.Header.EntryCount)
         {
             Debug.LogWarning("Asked to load texture at index " + EntryIndex + ", which is out of bounds!");
-            return;
+            return null;
         }
 
         alien_pak_entry Entry = AlienTextures.PAK.Entries[EntryIndex];
@@ -47,12 +48,12 @@ public class test2 : MonoBehaviour
         if (InTexture.Length_V1 == 0)
         {
             Debug.LogWarning("LENGTH ZERO - NOT LOADING");
-            return;
+            return null;
         }
         if (InTexture.Type == 7) 
         {
             Debug.LogWarning("CUBEMAP! NOT CURRENTLY SUPPORTED");
-            return;
+            return null;
         }
 
         UnityEngine.TextureFormat format = UnityEngine.TextureFormat.BC7;
@@ -69,28 +70,28 @@ public class test2 : MonoBehaviour
                 break;
             case alien_texture_format.Alien_FORMAT_SIGNED_DISTANCE_FIELD:
                 Debug.LogWarning("SDF! NOT LOADED");
-                return;
+                return null;
             case alien_texture_format.Alien_FORMAT_R8:
                 format = UnityEngine.TextureFormat.R8;
                 break;
             case alien_texture_format.Alien_FORMAT_BC1:
                 Debug.LogWarning("BC1! NOT LOADED");
-                return;
+                return null;
             case alien_texture_format.Alien_FORMAT_BC2:
                 Debug.LogWarning("BC2! NOT LOADED");
-                return;
+                return null;
             case alien_texture_format.Alien_FORMAT_BC5:
                 format = UnityEngine.TextureFormat.BC5;
                 break;
             case alien_texture_format.Alien_FORMAT_BC3:
                 Debug.LogWarning("BC3! NOT LOADED");
-                return;
+                return null;
             case alien_texture_format.Alien_FORMAT_BC7:
                 format = UnityEngine.TextureFormat.BC7;
                 break;
             case alien_texture_format.Alien_FORMAT_R8G8:
                 Debug.LogWarning("R8G8! NOT LOADED");
-                return;
+                return null;
         }
 
         Texture2D texture = new Texture2D(InTexture.Size_V1[0], InTexture.Size_V1[1], format, InTexture.MipLevelsV1, true);
@@ -100,9 +101,7 @@ public class test2 : MonoBehaviour
         texture.LoadRawTextureData(tempReader.ReadBytes(InTexture.Length_V1));
         tempReader.Close();
         texture.Apply();
-
-        uiSprite.preserveAspect = true;
-        if (uiSprite != null) uiSprite.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0));
+        return texture;
     }
 
     private GameObject LoadModel(int EntryIndex)
@@ -122,35 +121,32 @@ public class test2 : MonoBehaviour
             int BINIndex = ChunkArray.ChunkInfos[ChunkIndex].BINIndex;
             alien_model_bin_model_info Model = Result.ModelsBIN.Models[BINIndex];
             if (Model.BlockSize == 0) continue;
-
-            /*
-            alien_mtl_material InMaterial = Result.ModelsMTL.Materials[Model.MaterialLibraryIndex];
-            Debug.Log(InMaterial.UberShaderIndex);
-            Debug.Log(Result.ShadersIDXRemap.Datas.Count);
-            int RemappedIndex = Result.ShadersIDXRemap.Datas[InMaterial.UberShaderIndex].Index;
-            alien_shader_pak_shader Shader = Result.ShadersPAK.Shaders[RemappedIndex];
-
-            int TextureReferenceCount = Result.ModelsMTL.TextureReferenceCounts[Model.MaterialLibraryIndex];
-            int TableIndex = 11;
-            byte[] PairFromSlotTable = Shader.Tables[TableIndex];
-            int PairFromSlotTableSize = Shader.Header.TableEntryCounts[TableIndex];
-            for (int SlotIndex = 0; SlotIndex < PairFromSlotTableSize; ++SlotIndex)
-            {
-                int PairIndex = PairFromSlotTable[SlotIndex];
-                // NOTE: PairIndex == 255 means no index.
-                if (PairIndex < TextureReferenceCount)
-                {
-                    alien_mtl_texture_reference Pair = InMaterial.TextureReferences[PairIndex];
-                    alien_textures Textures = GetTexturesTable(Pair.TextureTableIndex);
-                    int TextureIndex = Pair.TextureIndex;
-                    alien_pak_entry Entry = Textures.PAK.Entries[TextureIndex];
-                    alien_texture_bin_texture InTexture = Textures.BIN.Textures[Entry.BINIndex];
-                    string TextureFilePath = Textures.BIN.TextureFilePaths[Entry.BINIndex];
-                    Debug.Log(TextureFilePath);
-                    alien_texture_format AlienFormat = (alien_texture_format)InTexture.Format;
-                }
-            }
-            */
+            
+            //Debug.Log(InMaterial.UberShaderIndex);
+            //Debug.Log(Result.ShadersIDXRemap.Datas.Count);
+            //int RemappedIndex = Result.ShadersIDXRemap.Datas[InMaterial.UberShaderIndex].Index;
+            //alien_shader_pak_shader Shader = Result.ShadersPAK.Shaders[RemappedIndex];
+            //
+            //int TextureReferenceCount = Result.ModelsMTL.TextureReferenceCounts[Model.MaterialLibraryIndex];
+            //int TableIndex = 11;
+            //byte[] PairFromSlotTable = Shader.Tables[TableIndex];
+            //int PairFromSlotTableSize = Shader.Header.TableEntryCounts[TableIndex];
+            //for (int SlotIndex = 0; SlotIndex < PairFromSlotTableSize; ++SlotIndex)
+            //{
+            //    int PairIndex = PairFromSlotTable[SlotIndex];
+            //    // NOTE: PairIndex == 255 means no index.
+            //    if (PairIndex < TextureReferenceCount)
+            //    {
+            //        alien_mtl_texture_reference Pair = InMaterial.TextureReferences[PairIndex];
+            //        alien_textures Textures = GetTexturesTable(Pair.TextureTableIndex);
+            //        int TextureIndex = Pair.TextureIndex;
+            //        alien_pak_entry Entry = Textures.PAK.Entries[TextureIndex];
+            //        alien_texture_bin_texture InTexture = Textures.BIN.Textures[Entry.BINIndex];
+            //        string TextureFilePath = Textures.BIN.TextureFilePaths[Entry.BINIndex];
+            //        Debug.Log(TextureFilePath);
+            //        alien_texture_format AlienFormat = (alien_texture_format)InTexture.Format;
+            //    }
+            //}
 
             alien_vertex_buffer_format VertexInput = Result.ModelsBIN.VertexBufferFormats[Model.VertexFormatIndex];
             alien_vertex_buffer_format VertexInputLowDetail = Result.ModelsBIN.VertexBufferFormats[Model.VertexFormatIndexLowDetail];
@@ -175,6 +171,12 @@ public class test2 : MonoBehaviour
             List<UInt16> InIndices = new List<UInt16>();
             List<Vector3> InVertices = new List<Vector3>();
             List<Vector3> InNormals = new List<Vector3>();
+            List<Vector4> InTangents = new List<Vector4>();
+            List<Vector2> InUVs0 = new List<Vector2>();
+            List<Vector2> InUVs1 = new List<Vector2>();
+            List<Vector2> InUVs2 = new List<Vector2>();
+            List<Vector2> InUVs3 = new List<Vector2>();
+            List<Vector2> InUVs7 = new List<Vector2>();
 
             for (int VertexArrayIndex = 0; VertexArrayIndex < Elements.Count; ++VertexArrayIndex)
             {
@@ -204,8 +206,10 @@ public class test2 : MonoBehaviour
                                                 InNormals.Add(Value);
                                                 break;
                                             case alien_vertex_input_slot.AlienVertexInputSlot_T:
+                                                InTangents.Add(new Vector4(Value.x, Value.y, Value.z, 0));
                                                 break;
                                             case alien_vertex_input_slot.AlienVertexInputSlot_UV:
+                                                //TODO: 3D UVW
                                                 break;
                                         };
                                         break;
@@ -217,6 +221,7 @@ public class test2 : MonoBehaviour
                                         switch (Input.ShaderSlot)
                                         {
                                             case alien_vertex_input_slot.AlienVertexInputSlot_C:
+                                                //??
                                                 break;
                                         }
                                         break;
@@ -228,6 +233,7 @@ public class test2 : MonoBehaviour
                                         switch (Input.ShaderSlot)
                                         {
                                             case alien_vertex_input_slot.AlienVertexInputSlot_BI:
+                                                //skinned mesh joints
                                                 break;
                                         }
                                         break;
@@ -240,8 +246,11 @@ public class test2 : MonoBehaviour
                                         switch (Input.ShaderSlot)
                                         {
                                             case alien_vertex_input_slot.AlienVertexInputSlot_BW:
+                                                //skinned mesh binding weights
                                                 break;
                                             case alien_vertex_input_slot.AlienVertexInputSlot_UV:
+                                                InUVs2.Add(new Vector2(Value.x, Value.y));
+                                                InUVs3.Add(new Vector2(Value.z, Value.w));
                                                 break;
                                         }
                                         break;
@@ -254,6 +263,16 @@ public class test2 : MonoBehaviour
                                         switch (Input.ShaderSlot)
                                         {
                                             case alien_vertex_input_slot.AlienVertexInputSlot_UV:
+                                                if (Input.VariantIndex == 0) InUVs0.Add(Value);
+                                                else if (Input.VariantIndex == 1)
+                                                {
+                                                    // TODO: We can figure this out based on alien_vertex_buffer_format_element.
+                                                    //Material->Material.Flags |= Material_HasTexCoord1;
+                                                    InUVs1.Add(Value);
+                                                }
+                                                else if (Input.VariantIndex == 2) InUVs2.Add(Value);
+                                                else if (Input.VariantIndex == 3) InUVs3.Add(Value);
+                                                else if (Input.VariantIndex == 7) InUVs7.Add(Value);
                                                 break;
                                         }
                                         break;
@@ -300,7 +319,19 @@ public class test2 : MonoBehaviour
             ThisModelPart.transform.parent = ThisModel.transform;
             ThisModelPart.transform.localScale = new Vector3(Model.ScaleFactor, Model.ScaleFactor, Model.ScaleFactor);
             ThisModel.name = Result.ModelsBIN.ModelFilePaths[BINIndex];
-            ThisModelPart.name = Result.ModelsBIN.ModelLODPartNames[BINIndex] + "(" + Result.ModelsMTL.MaterialNames[Model.MaterialLibraryIndex] + ")"; 
+            ThisModelPart.name = Result.ModelsBIN.ModelLODPartNames[BINIndex] + "(" + Result.ModelsMTL.MaterialNames[Model.MaterialLibraryIndex] + ")";
+
+
+            //TEST
+            List<alien_mtl_texture_reference> refs = Result.ModelsMTL.Materials[Model.MaterialLibraryIndex].TextureReferences.ToList();
+            Texture testTex = null;
+            for (int i = 0; i < refs.Count; i++)
+            {
+                if (refs[i].TextureTableIndex != 0 && refs[i].TextureTableIndex != 2) continue;
+                testTex = LoadTexture(refs[i].TextureIndex, refs[i].TextureTableIndex);
+                if (testTex) break;
+            }
+
 
             if (InVertices.Count == 0) continue;
 
@@ -308,11 +339,18 @@ public class test2 : MonoBehaviour
             thisMesh.SetVertices(InVertices);
             thisMesh.SetNormals(InNormals);
             thisMesh.SetIndices(InIndices, MeshTopology.Triangles, 0); //0??
+            thisMesh.SetTangents(InTangents);
+            thisMesh.SetUVs(0, InUVs0);
+            thisMesh.SetUVs(1, InUVs1);
+            thisMesh.SetUVs(2, InUVs2);
+            thisMesh.SetUVs(3, InUVs3);
+            thisMesh.SetUVs(7, InUVs7);
             thisMesh.RecalculateBounds();
             thisMesh.RecalculateNormals();
             thisMesh.RecalculateTangents();
             ThisModelPart.AddComponent<MeshFilter>().mesh = thisMesh;
             ThisModelPart.AddComponent<MeshRenderer>().material = new Material(UnityEngine.Shader.Find("Diffuse"));
+            ThisModelPart.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", testTex);
 
         }
 
@@ -332,14 +370,14 @@ public class test2 : MonoBehaviour
     }
 
     GameObject currentMesh = null;
-    int currentMeshIndex = 500;
+    int currentMeshIndex = 50;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
             if (currentMesh != null) Destroy(currentMesh);
-            //currentMesh = LoadModel(currentMeshIndex);
-            LoadTexture(currentMeshIndex);
+            currentMesh = LoadModel(currentMeshIndex);
+            //LoadTexture(currentMeshIndex);
             currentMeshIndex++;
         }
     }
