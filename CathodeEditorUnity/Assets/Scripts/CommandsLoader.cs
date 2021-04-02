43a6e1384529deace92fe9b4289c850a81b8e700
MattFiler <matthew.filer@hotmail.co.uk>
2021-04-02 20:47:06 +0100

    Imported CathodeLib updates & optimised

---
+++ CathodeEditorUnity/Assets/Scripts/CommandsLoader.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.IO;
+using System;
+using TestProject.File_Handlers.Commands;
+using CathodeLib;
+
+public class CommandsLoader : MonoBehaviour
+{
+    private CommandsPAK commandsPAK = null;
+    private List<alien_reds_entry> redsBIN;
+
+    UInt32 ModelReferenceID = 0;
+    void Start()
+    {
+        byte[] ModelReferenceIDBytes = new byte[4] { 0x94, 0xA8, 0xB4, 0xB9 };
+        ModelReferenceID = BitConverter.ToUInt32(ModelReferenceIDBytes, 0);
+    }
+
+    //Test code to load in everything that has a position: note, the hierarchy of objects needs to be considered here
+    public void LoadCommandsPAK(string LEVEL_NAME, List<alien_reds_entry> redsbin, System.Action<int, GameObject> loadModelCallback)
+    {
+        string basePath = LEVEL_NAME + "\\";
+        commandsPAK = new CommandsPAK(basePath + @"WORLD\COMMANDS.PAK");
+        redsBIN = redsbin;
+
+        for (int i = 0; i < commandsPAK.EntryPoints.Count; i++)
+        {
+            GameObject thisFlowgraphGO = new GameObject(commandsPAK.EntryPoints[i].name);
+            StartCoroutine(RecursiveLoad(commandsPAK.EntryPoints[i], thisFlowgraphGO, loadModelCallback));
+        }
+    }
+
+    private IEnumerator RecursiveLoad(CathodeFlowgraph flowgraph, GameObject parentTransform, System.Action<int, GameObject> loadModelCallback)
+    {
+        for (int i = 0; i < flowgraph.nodes.Count; i++)
+        {
+            CathodeNodeEntity node = flowgraph.nodes[i];
+            CathodeFlowgraph nextCall = commandsPAK.GetFlowgraph(node.nodeType);
+            if (nextCall == null) continue;
+            PosAndRot trans = GetTransform(ref node);
+            GameObject nextFlowgraphGO = new GameObject(nextCall.name);
+            nextFlowgraphGO.transform.parent = parentTransform.transform;
+            nextFlowgraphGO.transform.localPosition = trans.position;
+            nextFlowgraphGO.transform.localRotation = trans.rotation;
+            StartCoroutine(RecursiveLoad(nextCall, nextFlowgraphGO, loadModelCallback));
+        }
+
+        List<CathodeNodeEntity> models = GetAllOfType(ref flowgraph, ModelReferenceID);
+        for (int i = 0; i < models.Count; i++)
+        {
+            CathodeNodeEntity thisModel = models[i];
+
+            PosAndRot trans = GetTransform(ref thisModel);
+            GameObject thisNodeGO = new GameObject(NodeDB.GetNodeTypeName(thisModel.nodeType, ref commandsPAK) + ": " + NodeDB.GetFriendlyName(thisModel.nodeID));
+            thisNodeGO.transform.parent = parentTransform.transform;
+            thisNodeGO.transform.localPosition = trans.position;
+            thisNodeGO.transform.localRotation = trans.rotation;
+
+            LoadModelNode(ref thisModel, ref flowgraph, thisNodeGO, loadModelCallback);
+        }
+
+        /*
+        List<CathodeNodeEntity> sounds = GetAllOfType(flowgraph, new string[] { "SoundPlaybackBaseClass", "SoundObject", "Sound" });
+        foreach (CathodeNodeEntity node in sounds)
+        {
+            PosAndRot thisNodePos = GetTransform(node) + stackedTransform;
+            GameObject newCube = new GameObject(NodeDB.GetName(node.nodeType));
+            newCube.transform.position = new Vector3(thisNodePos.position.x, thisNodePos.position.y, thisNodePos.position.z);
+            newCube.transform.eulerAngles = new Vector3(thisNodePos.rotation.x, thisNodePos.rotation.y, thisNodePos.rotation.z);
+            newCube.AddComponent<AudioSource>();
+            continue;
+        }
+
+        List<CathodeNodeEntity> particles = GetAllOfType(flowgraph, new string[] { "ParticleEmitterReference", "RibbonEmitterReference", "GPU_PFXEmitterReference" });
+        foreach (CathodeNodeEntity node in particles)
+        {
+            PosAndRot thisNodePos = GetTransform(node) + stackedTransform;
+            GameObject newCube = new GameObject(NodeDB.GetName(node.nodeType));
+            newCube.transform.position = new Vector3(thisNodePos.position.x, thisNodePos.position.y, thisNodePos.position.z);
+            newCube.transform.eulerAngles = new Vector3(thisNodePos.rotation.x, thisNodePos.rotation.y, thisNodePos.rotation.z);
+            newCube.AddComponent<ParticleSystem>().emissionRate = 0;
+            continue;
+        }
+        */
+
+        /*
+        List<CathodeNodeEntity> lights = GetAllOfType(flowgraph, new string[] { "LightReference" });
+        foreach (CathodeNodeEntity node in lights)
+        {
+            PosAndRot trans = GetTransform(node);
+            GameObject thisNodeGO = new GameObject(NodeDB.GetNodeTypeName(node.nodeType, commandsPAK) + ": " + NodeDB.GetFriendlyName(node.nodeID));
+            thisNodeGO.transform.parent = parentTransform.transform;
+            thisNodeGO.transform.localPosition = trans.position;
+            thisNodeGO.transform.localRotation = trans.rotation;
+            thisNodeGO.AddComponent<Light>(); //todo: pull properties from game
+        }
+        */
+
+        yield break;
+    }
+
+    private List<CathodeNodeEntity> GetAllOfType(ref CathodeFlowgraph flowgraph, UInt32 nodeType)
+    {
+        return flowgraph.nodes.FindAll(o => o.nodeType == nodeType);
+    }
+
+    private PosAndRot GetTransform(ref CathodeNodeEntity node)
+    {
+        PosAndRot toReturn = new PosAndRot();
+        foreach (CathodeParameterReference paramRef in node.nodeParameterReferences)
+        {
+            CathodeParameter param = commandsPAK.GetParameter(paramRef.offset);
+            if (param == null) continue;
+            if (param.dataType != CathodeDataType.POSITION) continue;
+            CathodeTransform transform = (CathodeTransform)param;
+            toReturn.position = transform.position;
+            toReturn.rotation = Quaternion.Euler(transform.rotation);
+            break;
+        }
+        return toReturn;
+    }
+
+    private List<UInt32> GetResource(ref CathodeNodeEntity node)
+    {
+        List<UInt32> resources = new List<UInt32>();
+        foreach (CathodeParameterReference paramRef in node.nodeParameterReferences)
+        {
+            CathodeParameter param = commandsPAK.GetParameter(paramRef.offset);
+            if (param == null) continue;
+            if (param.dataType != CathodeDataType.SHORT_GUID) continue;
+            resources.Add(((CathodeResource)param).resourceID);
+        }
+        return resources;
+    }
+
+    private void LoadModelNode(ref CathodeNodeEntity node, ref CathodeFlowgraph flowgraph, GameObject thisNodeGO, System.Action<int, GameObject> loadModelCallback)
+    {
+        List<UInt32> resourceID = GetResource(ref node);
+        for (int i = 0; i < resourceID.Count; i++)
+        {
+            List<CathodeResourceReference> resRef = flowgraph.GetResourceReferencesByID(resourceID[i]);
+            for (int x = 0; x < resRef.Count; x++)
+            {
+                if (resRef[x].entryType != CathodeResourceReferenceType.RENDERABLE_INSTANCE) continue; //Ignoring collision maps, etc, for now
+                for (int p = 0; p < resRef[x].entryCountREDS; p++) loadModelCallback(redsBIN[resRef[x].entryIndexREDS].ModelIndex + p, thisNodeGO);
+            }
+        }
+    }
+}
+
+public class PosAndRot
+{
+    public static PosAndRot operator+ (PosAndRot a, PosAndRot b)
+    {
+        PosAndRot newTrans = new PosAndRot();
+        newTrans.position = a.position + b.position;
+        newTrans.rotation = a.rotation * b.rotation;
+        return newTrans;
+    }
+
+    public Vector3 position = new Vector3();
+    public Quaternion rotation = new Quaternion();
+}
---
+++ CathodeEditorUnity/Assets/Scripts/NodeDB.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TestProject.File_Handlers.Commands;
+using UnityEngine;
+
+namespace CathodeLib
+{
+    public class ShortGUIDDescriptor
+    {
+        public UInt32 ID;
+        public string Description;
+    }
+
+    public class NodeDB
+    {
+        static NodeDB()
+        {
+            cathode_id_map = ReadDB(File.ReadAllBytes(Application.streamingAssetsPath + "/NodeDBs/cathode_id_map.bin")); //Names for node types, parameters, and enums
+            node_friendly_names = ReadDB(File.ReadAllBytes(Application.streamingAssetsPath + "/NodeDBs/node_friendly_names.bin")); //Names for unique nodes
+        }
+
+        //Check the CATHODE data dump for a corresponding name
+        public static string GetName(UInt32 id)
+        {
+            if (id == 0) return "";
+            foreach (ShortGUIDDescriptor db_entry in cathode_id_map) if (db_entry.ID == id) return db_entry.Description;
+            return id.ToString();
+        }
+        public static string GetNodeTypeName(UInt32 id, ref CommandsPAK pak) //This is performed separately to be able to remap nodes that are flowgraphs
+        {
+            if (id == 0) return "";
+            foreach (ShortGUIDDescriptor db_entry in cathode_id_map) if (db_entry.ID == id) return db_entry.Description;
+            CathodeFlowgraph flow = pak.GetFlowgraph(id); if (flow == null) return id.ToString();
+            return flow.name;
+        }
+
+        //Check the COMMANDS.BIN dump for node in-editor names
+        public static string GetFriendlyName(UInt32 id)
+        {
+            if (id == 0) return "";
+            foreach (ShortGUIDDescriptor db_entry in node_friendly_names) if (db_entry.ID == id) return db_entry.Description;
+            return id.ToString();
+        }
+
+        private static List<ShortGUIDDescriptor> ReadDB(byte[] db_content)
+        {
+            List<ShortGUIDDescriptor> toReturn = new List<ShortGUIDDescriptor>();
+
+            MemoryStream readerStream = new MemoryStream(db_content);
+            BinaryReader reader = new BinaryReader(readerStream);
+            while (reader.BaseStream.Position < reader.BaseStream.Length)
+            {
+                ShortGUIDDescriptor thisDesc = new ShortGUIDDescriptor();
+                thisDesc.ID = reader.ReadUInt32();
+                thisDesc.Description = reader.ReadString();
+                toReturn.Add(thisDesc);
+            }
+            reader.Close();
+
+            return toReturn;
+        }
+
+        private static List<ShortGUIDDescriptor> cathode_id_map;
+        private static List<ShortGUIDDescriptor> node_friendly_names;
+    }
+}
