f2cfa8b8dfe1ec98114bfc776d7d6a49fc5edeba
MattFiler <matthew.filer@hotmail.co.uk>
2023-01-17 23:19:56 +0000

    test

--- CathodeEditorUnity/Assets/ModelTester.cs
+++ CathodeEditorUnity/Assets/ModelTester.cs
@@ -4,24 +4,39 @@ using static CATHODE.Models;
 
 public class ModelTester : MonoBehaviour
 {
-    int i = 30;
+    int i = 50;
     GameObject modelGO = null;
     Models mdls = null;
     private void Start()
     {
         mdls = new Models("G:\\SteamLibrary\\steamapps\\common\\Alien Isolation\\DATA\\ENV\\PRODUCTION\\SOLACE\\RENDERABLE\\LEVEL_MODELS.PAK");
+        LoadModel();
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
         {
-            if (modelGO != null)
-                Destroy(modelGO);
+            i++;
+            LoadModel();
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            i--;
+            LoadModel();
+        }
+    }
 
-            CS2 model = mdls.Entries[i];
-            Debug.Log("[" + model.Submeshes.Count + "] " + model.Name);
-            modelGO = new GameObject(model.Name);
-            for (int x = 0; x < model.Submeshes.Count; ++x)
+    private void LoadModel()
+    {
+        if (modelGO != null)
+            Destroy(modelGO);
+
+        CS2 model = mdls.Entries[i];
+        Debug.Log("[" + model.Submeshes.Count + "] " + model.Name);
+        modelGO = new GameObject(model.Name);
+        for (int x = 0; x < model.Submeshes.Count; ++x)
+        {
+            try
             {
                 GameObject submeshGO = new GameObject();
                 submeshGO.transform.parent = modelGO.transform;
@@ -29,7 +44,10 @@ public class ModelTester : MonoBehaviour
                 submeshGO.AddComponent<MeshFilter>().mesh = mdls.GetMesh(model.Submeshes[x]);
                 submeshGO.AddComponent<MeshRenderer>().material = new Material(Shader.Find("Standard"));
             }
-            i++;
+            catch (System.Exception e)
+            {
+                Debug.LogError(e.ToString());
+            }
         }
     }
 }
